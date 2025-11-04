967643f8979af352051259caec7c25a68118ccea
Julia2063 <sky206380@gmail.com.com>
2025-11-04 17:42:12 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,42 @@
 'use strict';
+/* eslint-disable no-console */
+
+const { copyFile } = require('fs/promises');
+
+async function app() {
+  const args = process.argv.slice(2);
+
+  let source;
+  let destination;
+
+  if (args[0] === 'cp') {
+    [, source, destination] = args;
+  } else {
+    [source, destination] = args;
+  }
+
+  if (!source || !destination) {
+    console.error("I can just copy file with command 'cp'!");
+
+    return;
+  }
+
+  if (source === destination) {
+    console.error('You try to copy to the same location!');
+
+    return;
+  }
+
+  try {
+    await copyFile(source, destination);
+    console.log(`${source} was copied to ${destination}`);
+  } catch {
+    console.error('The file could not be copied');
+  }
+}
+
+app();
+
+module.exports = {
+  app,
+};
